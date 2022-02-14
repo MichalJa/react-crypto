ee5bc395f26bbfbd29ac2db767421113e48f9a04
MichalJa <96233129+MichalJa@users.noreply.github.com>
2022-02-14 11:30:52 +0100

    Update CryptoList.js

--- src/CryptoList.js
+++ src/CryptoList.js
@@ -6,40 +6,42 @@ function CryptoList(props) {
 
     let listElements = cryptoList.map((cryptoObject) => {
         let symbol = cryptoObject.symbol;
+        const transformedSymbol = 
+            symbol === 'USD' ? '$': 
+            symbol === 'AUD' ? '$': 
+            symbol === 'BRL' ? 'R$': 
+            symbol === 'CAD' ? '$': 
+            symbol === 'CHF' ? 'CHF': 
+            symbol === 'CLP' ? '$': 
+            symbol === 'CNY' ? '¥': 
+            symbol === 'CZK' ? 'Kč': 
+            symbol === 'DKK' ? 'kr': 
+            symbol === 'EUR' ? '€': 
+            symbol === 'GBP' ? '£': 
+            symbol === 'HKD' ? 'HK$ / 元': 
+            symbol === 'HRK' ? 'kn': 
+            symbol === 'HUF' ? 'ft': 
+            symbol === 'INR' ? '₹': 
+            symbol === 'ISK' ? 'kr': 
+            symbol === 'JPY' ? '¥': 
+            symbol === 'KRW' ? '₩': 
+            symbol === 'PLN' ? 'zł': 
+            symbol === 'RON' ? 'lei': 
+            symbol === 'RUB' ? '₽': 
+            symbol === 'SEK' ? 'kr': 
+            symbol === 'SGD' ? '$': 
+            symbol === 'THB' ? '฿': 
+            symbol === 'TRY' ? '₺': 
+            symbol === 'TWD' ? 'NT$': 
+            symbol === 'NZD' ? '$': 
+            String.fromCharCode(9888) ;
+
         return (
             <li key={cryptoObject.currency}>
                 <span className="LastRate">Last rate:</span>
                 <span className={`CryptoRate ${cryptoObject.cssClass}`}>{cryptoObject.lastRate} {cryptoObject.htmlArray}</span>
                 <span className="Ticker">{cryptoObject.currency}</span>
-                <span className="Symbol">[{
-                symbol === 'USD' ? '$': 
-                symbol === 'AUD' ? '$': 
-                symbol === 'BRL' ? 'R$': 
-                symbol === 'CAD' ? '$': 
-                symbol === 'CHF' ? 'CHF': 
-                symbol === 'CLP' ? '$': 
-                symbol === 'CNY' ? '¥': 
-                symbol === 'CZK' ? 'Kč': 
-                symbol === 'DKK' ? 'kr': 
-                symbol === 'EUR' ? '€': 
-                symbol === 'GBP' ? '£': 
-                symbol === 'HKD' ? 'HK$ / 元': 
-                symbol === 'HRK' ? 'kn': 
-                symbol === 'HUF' ? 'ft': 
-                symbol === 'INR' ? '₹': 
-                symbol === 'ISK' ? 'kr': 
-                symbol === 'JPY' ? '¥': 
-                symbol === 'KRW' ? '₩': 
-                symbol === 'PLN' ? 'zł': 
-                symbol === 'RON' ? 'lei': 
-                symbol === 'RUB' ? '₽': 
-                symbol === 'SEK' ? 'kr': 
-                symbol === 'SGD' ? '$': 
-                symbol === 'THB' ? '฿': 
-                symbol === 'TRY' ? '₺': 
-                symbol === 'TWD' ? 'NT$': 
-                symbol === 'NZD' ? '$': 
-                String.fromCharCode(9888) }]</span>
+                <span className="Symbol">[{transformedSymbol}]</span>
             </li>
         );
     });
